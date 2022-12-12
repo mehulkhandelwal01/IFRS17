5c558f6dd262a8144d86cb147c83b1f428ab9e48
patrickm663 <rev0cation2025@outlook.com>
2022-12-12 13:58:35 +0200

    Fix: Upload csv via Streamlit

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ assumptions = st.file_uploader('Choose an Assumptions CSV File')
 parameters = st.file_uploader('Choose an Parameters CSV File')
 
 if assumptions is not None and parameters is not None:
-
+    assumptions = pd.read_csv(assumptions)
+    parameters = pd.read_csv(parameters)
+    st.write(assumptions)
+    st.write(parameters)
     g = GMM(assumptions, parameters)
     st.table(g.Analysis_by_remaining_coverage)
