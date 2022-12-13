26e26000137e9600e04ee18795713f477f69a22f
patrickm663 <rev0cation2025@outlook.com>
2022-12-13 12:14:32 +0200

    Fix: remove hamburger menu

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ from IFRS17.gmm import GMM
 
 st.set_page_config(page_title="Actuartech IFRS 17 Data Management")
 
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 st.title("Actuartech IFRS 17 Data Management Software")
 
 @st.cache
@@ -35,7 +42,7 @@ with load_tab:
             #st.subheader('Dashboard')
             recon_container = st.container()
             recon_container.write('#### Reconciliation Account')
-            recon = recon_container.selectbox('', ('Reconciliation of Best Estimate Liability', 'Reconciliation of Contractual Service Margin', 'Reconciliation of Risk Adjustment', 'Reconciliation of Total Contract Liability'))
+            recon = recon_container.selectbox('#### ', ('Reconciliation of Best Estimate Liability', 'Reconciliation of Contractual Service Margin', 'Reconciliation of Risk Adjustment', 'Reconciliation of Total Contract Liability'))
             year_col, measure_col, table_col, recon_col = st.columns(4)
             with year_col:
                 st.write('#### Year')
@@ -51,9 +58,9 @@ with load_tab:
 
             min_year = np.min(data.index)
             max_year = np.max(data.index)
-            years = st.slider('Year:', min_year, max_year, (min_year, min_year+1))
+            years = st.slider('#### ', min_year, max_year, (min_year, min_year+1))
             year_range = range(years[0], years[len(years)-1]+1)
-            #with table_col:
+
             st.write(str('#### ' + str(recon)))
             st.dataframe(data.loc[year_range, :], use_container_width=True) 
 
@@ -61,7 +68,7 @@ with load_tab:
             container_measure.write('#### Measure')
             measure_values = data.columns
             list_of_measures = tuple(measure_values[2:len(measure_values)])
-            measure = container_measure.radio("", list_of_measures)
+            measure = container_measure.radio("#### ", list_of_measures)
 
             graph_measure = st.container()
             graph_measure.write('#### Graph')
