a470d4c52f4f9970d33c6811d139762a79d3c689
mehulkhandelwal01 <121025440+mehulkhandelwal01@users.noreply.github.com>
2023-01-19 16:49:16 +0530

    Update rds.py

--- rds.py
+++ rds.py
@@ -22,7 +22,7 @@ def run_query(query):
 rows = run_query("SELECT * from BEL;")
 headers = run_query("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'BEL';")
 
-st.write(pd.DataFrame(rows,columns=[[headers]]))
+st.write(pd.DataFrame(rows,columns=['headers']))
 
 # Print results.
 # Print results.
